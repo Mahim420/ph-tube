a3a2a93d554168369f0e6b1d95b97f4a1fbdbc49
mdmahimkhan77250@gmail.com <your_email@example.com>
2026-01-21 11:02:39 +0600

    Implement navigations system

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,26 @@
-const loadAllData = async () => {
-    const res = await fetch("https://openapi.programming-hero.com/api/phero-tube/categories");
+// Remove class
+const removeClass = () => {
+    const allActiveClass = document.getElementsByClassName('active');
+    for (let btn of allActiveClass) {
+        btn.classList.remove('active');
+    }
+}
 
-    const data = await res.json();
-    displaData(data.categories);
-};
 
 
-const displaData = (categories) => {
+// Display NavData
+const displayNavData = (categories) => {
     const navItem = document.getElementById('nav-item');
     categories.forEach(element => {
         const li = document.createElement('li');
-        li.innerHTML = `<button onclick="categoryWiseVideo(${element.category_id})" class="btn btn-sm">${element.category}</button>`;
+        li.innerHTML = `<button id="btn-${element.category_id}" onclick="categoryWiseVideo(${element.category_id})" class="btn btn-sm">${element.category}</button>`;
 
         navItem.appendChild(li);
     });
+
 };
 
-const loadVideo = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/videos');
-    const data = await res.json();
-    displayVideo(data.videos);
-};
-
-
-const categoryWiseVideo = async (id) => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`);
-    const data = await res.json();
-    displayVideo(data.category)
-};
-
+// Display Video Functions
 const displayVideo = (videos) => {
 
     const videoSections = document.getElementById("video-sections");
@@ -49,7 +41,7 @@ const displayVideo = (videos) => {
 
     else {
         videos.forEach(el => {
-            // console.log(el)
+            console.log(el)
             const newCard = document.createElement('div');
             newCard.innerHTML = `
         <div class="card bg-base-100 h-80 w-72 shadow-sm">
@@ -71,22 +63,55 @@ const displayVideo = (videos) => {
                             <h2 class="card-title">
                                 ${el.title}
                             </h2>
-                            <p class="flex gap-1 items-center">
+                            <p id="verified" class="flex gap-1 items-center">
                                 <span>${el.authors[0].profile_name}</span>
-                                <span>
-                                    <img class="w-5 h-5" src="./assets/v.png" alt="">
-                                </span>
+                                ${el.authors[0].verified ?
+                    `<span>
+                                         <img class="w-5 h-5" src="./assets/v.png" alt="">
+                                    </span>`
+                    : ''}
                             </p>
                             <p class="flex items-center gap-2"><span>${el.others.views}</span> <span>views</span></p>
                         </div>
                     </div>
+                     <button class="btn mt-7 btn-block">View Detailes</button>
                 </div>
             </div>
         `;
-
             videoSections.appendChild(newCard);
         })
     }
 }
+
+
+// Loading Nav data
+const loadAllData = async () => {
+    const res = await fetch("https://openapi.programming-hero.com/api/phero-tube/categories");
+
+    const data = await res.json();
+    displayNavData(data.categories);
+};
+
+// Load all allsections Video
+const loadVideo = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phero-tube/videos');
+    const data = await res.json();
+    removeClass();
+    const firstBtn = document.getElementById('first-button');
+    firstBtn.classList.add('active');
+    displayVideo(data.videos);
+};
+
+
+// Loading Categories Video
+const categoryWiseVideo = async (id) => {
+    removeClass();
+    const navIndicator = document.getElementById(`btn-${id}`)
+    navIndicator.classList.add('active');
+    const res = await fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`);
+    const data = await res.json();
+    displayVideo(data.category);
+};
+
 
 loadAllData();
